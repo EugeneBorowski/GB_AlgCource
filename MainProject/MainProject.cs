8291dfca812ea1d8d8bfbc2dc7602da2009758e5
Eugene <e.borovskiy@yandex.ru>
2021-12-06 20:11:25 +0300

    done
    Во 2 задании сложность O(N^3)

---
+++ MainProject/MainProject.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using MainProject.Lesson_1;
+
+
+namespace MainProject
+{
+    class MainProject
+    {
+        static void Main(string[] args)
+        {
+            //Задание 1-1
+            Console.WriteLine("Задание 1");
+            Console.WriteLine(Lesson1_1.IsPrime(7));
+            Console.WriteLine(Lesson1_1.IsPrime(9));
+            //Задание 1-3
+            Console.WriteLine("Задание 3, число фибоначчи рекурсией и обычным методом");
+            Console.WriteLine(Lesson1_3.GetFibonacci(10));
+            Console.WriteLine(Lesson1_3.GetFibonacci2(10));
+        }
+    }
+}
