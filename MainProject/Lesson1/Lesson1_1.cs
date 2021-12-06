4327c4e72dd9b30913ca1b72fa11ea23997c721b
Eugene <e.borovskiy@yandex.ru>
2021-12-06 20:11:25 +0300

    done
    Во 2 задании сложность O(N^3)

---
+++ MainProject/Lesson1/Lesson1_1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace MainProject.Lesson_1
+{
+    class Lesson1_1
+    {
+        public static string IsPrime(int number)
+        {
+            var d = 0;
+            var i = 2;
+            while (i < number)
+            {
+                if (number % i == 0)
+                    d++;
+                i++;
+            }
+            var result = d == 0 ? $"{number} простое" : $"{number} не простое";
+            return result;
+        }
+    }
+}
---
+++ MainProject/MainProject.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading.Channels;
+using MainProject.Lesson_1;
+
+
+namespace MainProject
+{
+    class MainProject
+    {
+        static void Main()
+        {
+            //Задание 1-1
+            Console.WriteLine("Задание 1");
+            Console.WriteLine(Lesson1_1.IsPrime(7));
+            Console.WriteLine(Lesson1_1.IsPrime(9));
+            //Задание 1-2
+            Console.WriteLine("Задание 2, сложность O(N^3)");
+            var l1_2 = new[] { 1, 2, 3, 4, 5 };
+                Console.WriteLine(Lesson1_2.StrangeSum(l1_2));
+                //Задание 1-3
+            Console.WriteLine("Задание 3, число фибоначчи рекурсией и обычным методом");
+            Console.WriteLine(Lesson1_3.GetFibonacci(10));
+            Console.WriteLine(Lesson1_3.GetFibonacci2(10));
+        }
+    }
+}
